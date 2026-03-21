38d849be8048f4b41e69aa67f162611765d1399d
Joshua Goins <josh@redstrate.com>
2026-03-21 19:01:31 -0400

    Fix mistake in Flatpak repo script
    (cherry picked from commit c7cc0a9749323f79ddd90d91495758ecbcf20512)

--- scripts/copy-repo-to-server.sh
+++ scripts/copy-repo-to-server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rsync -e "ssh -p 38901 -o StrictHostKeyChecking=no" --recursive /home/josh/sources/flatpak-distrib/ ryne.moe:/srv/http/umbra-distrib/flatpak
+rsync -e "ssh -p 38901 -o StrictHostKeyChecking=no" --recursive /home/josh/sources/flatpak-distrib/ ryne.moe:/srv/http/astra-distrib/flatpak
